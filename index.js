d4145550de1720c8723199cbb5c823f3aa1f1ba4
github username <github email address>
2019-02-23 14:33:28 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,81 @@ const questions = [
 ]
 
 let question;
+// let hidden = false;
+
+function appendQuestion(questions){
+  const qContainer = document.querySelector('.question-container')
+  qContainer.innerHTML = questions.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve(question)
+    }, time )
+  })
+}
+
+function removeQuestion(){
+  const qContainer = document.querySelector('.question-container')
+  qContainer.innerHTML = ""
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  let buttonContainer = document.querySelector('.true-false-list')
+  let buttons = buttonContainer.querySelectorAll('.btn')
+  return buttons
+}
+
+// function toggleTrueAndFalseButtons(){
+//   let buttonContainer = document.querySelector('.true-false-list')
+//   let buttons = buttonContainer.querySelectorAll('.btn')
+//     buttons.forEach(function(e){
+//     if (hidden == false){
+//       debugger
+//       e.classList = "hide"
+//       hidden = true
+//     } else if (hidden == true) {
+//       e.classList = ""
+//       hidden = false
+//       }
+//   })
+// }
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(e){
+        e.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  let buttons = document.querySelector(".waves-effect")
+  buttons.addEventListener("click", function () {
+  toggleTrueAndFalseButtons()
+  askQuestionThenRemoveQuestion(1000)
+  })
+}
+
+
+// function toggleTrueAndFalseButtons(){
+//   let buttonContainer = document.querySelector('.true-false-list')
+//   let buttons = buttonContainer.querySelectorAll('.btn')
+//     buttons.forEach(function(e){
+//     if (hidden == false){
+//       // e.classList = `${e.classList}` + ` hide`
+//       e.classList.value = `${e.classList.value}` + ` hide`
+//       hidden = true
+//     } else if (hidden == true){
+//       e.classList.value = `${e.classList.value}` - ` hide`
+//       debugger
+//       hidden = false
+//       }
+//     })
+//   // })
+// }
